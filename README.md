b86397d40912c8da059f902bde2ac236efc6bc99
VINEET PRIYANSHU <74959291+vineet-priyanshu@users.noreply.github.com>
2020-12-03 14:26:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# unknown
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  car=createSprite(50, 200, 50, 50);
+  wall=createSprite(1500,200,60,height/2);
+  
+  speed=random(52,90);
+  weight=random(200,1500);
+  car.velocityX=speed;
+}
+
+function draw() {
+  background(255,255,255);  
+
+ if(wall.x-car.x < car.width/2 + wall.width/2){
+   car.velocityx=0;
+   var deformation=0.5 * weight * speed* speed/22500;
+   if(deformation>180){
+     car.shapeColor=color(255,0,0);
+   }
+   if(deformation<180 && deformation>100){
+     car.shapeColor=color(230,230,0);
+   }
+   if(deformation<100){
+     car.shapeColor=color(0,255,0);
+   }
+ }
+
+  drawSprites();
+}
